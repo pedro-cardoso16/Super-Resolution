5823b23e92a77f7144539494706ea2aae2da9451
pedro-cardoso16 <pedrohpcardoso16@gmail.com>
2025-02-19 23:28:58 +0100

    ok

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 https://data.vision.ee.ethz.ch/cvl/DIV2K/
 
+## Getting Started
+In order to train the model you can run the following command in the project directory
 ```shell
-python3 super-resolve train
+python3 super-resolve train ./dataset/BSDS300/images/train --batch-size 32 --epoch 30 -S .models --use-cuda 
 ```
+Once the model is trained you can excute it using
+```shell
+python3 super-resolve <image> -m <model> -o <output-image>
+```
+
 
--- super-resolve.py
+++ super-resolve.py
@@ -1,14 +1,9 @@
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-import torch, torchvision, torcheval
 import os, sys, re
 
 from supresolv.data import train_model, process_image
-
-
-from tqdm import tqdm
-from torch.utils.data import DataLoader
 
 # -- Main parser -------------------------------------------------------------
 
@@ -65,15 +60,98 @@ if __name__ == "__main__":
 
         case _: # default case
             args, unknown = parser.parse_known_args()
-           
+
             process_image(args.image, args.model, args.output)
-
-
-
-
 
 
     # if args.command == 'train':
     #     print('train called')
 
     # args, unknown = train_parser.parse_known_args()
+
+# import argparse
+# import logging
+# import os
+# import sys
+# import torch
+# from supresolv.data import train_model, process_image
+
+# # Configure logging
+# logging.basicConfig(
+#     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+# )
+
+# # -- Main parser -------------------------------------------------------------
+# parser = argparse.ArgumentParser(
+#     prog="super-resolve",
+#     description="CNN model for super resolution of images",
+# )
+# parser.add_argument("image", nargs="?", help="IMAGE file to super resolve")
+# parser.add_argument(
+#     "-m", "--model", required=False, help="Load MODEL for super resolution"
+# )
+# parser.add_argument("-o", "--output", help="OUTPUT image")
+
+# subparsers = parser.add_subparsers(help="Sub-type", dest="command")
+
+# # -- Train subparser ---------------------------------------------------------
+# train_parser = subparsers.add_parser("train", help="Train a super-resolution model")
+# train_parser.add_argument("train_folder", type=str, help="Path to the training dataset")
+# train_parser.add_argument(
+#     "-u", "--upscale-factor", type=int, default=3, help="Upscale factor"
+# )
+# train_parser.add_argument(
+#     "-S",
+#     "--save-folder",
+#     default="./",
+#     type=str,
+#     help="Save models to destination folder",
+# )
+# train_parser.add_argument(
+#     "-e", "--epochs", type=int, default=30, help="Number of epochs for training"
+# )
+# train_parser.add_argument(
+#     "-b", "--batch-size", type=int, default=256, help="Batch size"
+# )
+# train_parser.add_argument(
+#     "--lr", "--learning-rate", type=float, default=0.001, help="Learning rate"
+# )
+# train_parser.add_argument(
+#     "--use-cuda", action="store_true", help="Use CUDA if available"
+# )
+
+# if __name__ == "__main__":
+#     if len(sys.argv) < 2:
+#         parser.print_help()
+#         sys.exit(1)
+
+#     args = parser.parse_args()
+#     device = torch.device(
+#         "cuda" if args.use_cuda and torch.cuda.is_available() else "cpu"
+#     )
+
+#     if args.command == "train":
+#         logging.info("Starting training...")
+#         train_model(
+#             epochs=args.epochs,
+#             upscale_factor=args.upscale_factor,
+#             train_folder=args.train_folder,
+#             save_folder=args.save_folder,
+#             device=device,
+#             batch_size=args.batch_size,
+#         )
+#     else:
+#         if not args.image:
+#             logging.error(
+#                 "Error: No image file provided. Please specify an image file."
+#             )
+#             sys.exit(1)
+#         if not args.model:
+#             logging.error(
+#                 "Error: No model file provided. Use -m or --model to specify the model path."
+#             )
+#             sys.exit(1)
+
+#         logging.info("Processing image...")
+#         process_image(args.image, args.model, args.output)
+#         logging.info("Image processing complete.")
